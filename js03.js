f7f4cf7e49636cb79697898921d0201dcf7d52ad
MothtatoSoup <maddy.puryear@outlook.com>
2025-02-04 13:07:05 -0500

    page 92

--- js03.js
+++ js03.js
@@ -30,3 +30,28 @@ function addWeekDays() {
         i++;
     }
 }
+
+window.addEventListener("load", showGames);
+
+//func to write game info in calenders
+function showGames(){
+    for (let i = 0; i < gameDates.length; i++){
+        let gameInfo = "";
+
+        //Open paragraph
+        gameInfo += "<p>";
+
+        //Include the opponent
+        gameInfo += gameOpponents[i] + "<br>";
+
+        //Include the result and score
+        gameInfo += gameResults[i] + ": ( "+ runsScored[i] + " -  "+ runsAllowed[i] +" ) ";
+
+        //Close the paragraph
+        gameInfo += "</p>";
+
+        //Write the info into a table cell
+        let tabelCell = document.getElementById(gameDates[i]);
+        tabelCell.insertAdjacentHTML("beforeEnd", gameInfo)
+    }
+}
