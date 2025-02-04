03825492146f4a2caf61fa3d849ff34e32bbf166
MothtatoSoup <maddy.puryear@outlook.com>
2025-02-04 12:57:43 -0500

    page 88

--- js03.js
+++ js03.js
@@ -11,4 +11,22 @@
  */
 
 // Days of the week
-let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+window.addEventListener("load", addWeekDays);
+
+// FUnc to write weekday names int the calender
+function addWeekDays() {
+    let i = 0; // initial counter val
+
+    //refrence the collection of heading cells
+    let headingCells = document.getElementsByTagName("th");
+
+    // write each of the 7 days into a heading cells
+    while(1 < 7){
+        headingCells[i].innerHTML = weekDays[i];
+
+        // counter + 1
+        i++;
+    }
+}
